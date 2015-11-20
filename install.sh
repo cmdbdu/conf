c18bd0d662ecc2dba6741dab170c738c0662de6d
cmdbdu <cmdbdu@gmail.com>
2015-11-20 10:50:38 +0800

    添加自己的TMUX 配置 完成简单新机安装配置脚本

---
+++ install.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+tools="git zsh tmux vim"
+
+check() {
+    for tool in $tools
+        do
+            if dpkg -l | grep $tool >/dev/null 2>&1;then
+                echo "$tool was installed "
+            else
+                echo "starting install $tool"
+                # sudo apt-get install $tool
+                echo "Install $tool"
+            fi
+    
+        done
+    }
+clone_private_dotfile() {
+    
+    cd
+    if [ -d conf ];then
+        echo "conf was cloned"
+    else
+       # git clone http://github.com/cmdbdu/conf.git
+        echo "clone conf "
+    fi
+    }
+
+# 安装vim 插件管理器 NeoBundle
+install_neobundle() {
+    echo "install neobundle"
+    if [ -d ~/.vim/bundle ];then
+        echo "dir was esxit"
+    else
+        echo "mkdir ~/.vim/bundle"
+        #mkdir -p ~/.vim/bundle
+    fi
+    #git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+    
+    } 
+
+# 配置VIM
+conf_vim() {
+    echo "conf_vim"
+    
+    if [ -d .vim ];then
+        echo ".vim esxit"
+    else
+       # mkdir .vim
+       # cp ~/conf/vimrc .vim   
+       echo "cp ~/conf/vimrc .vim. "
+    fi
+
+    }
+
+
+# 配置TMUX
+conf_tmux() {
+    
+    if [ -f ~/.tmux.conf ];then
+        echo "file was esxit"
+    else
+       # cp ~/conf/.tmux.conf ~
+       echo "cp ~/conf/.tmux.conf ~"
+    fi
+
+    }
+
+# 安装oh-my-zsh
+conf_zsh() {
+    
+    #git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    #cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+    #chsh -s /bin/zsh
+    
+    echo 'zsh'
+    }
+
+conf_tools() {
+    check
+    clone_private_dotfile
+    install_neobundle
+    conf_vim
+    conf_tmux
+    conf_zsh
+    }
+
+conf_tools
+
+echo "ALL THINGS WAS DONE !!!"
